97646930f34b8dfc7813be2d5fbddc8347a26854
Sined <denic_b03@mail.ru>
2025-03-09 23:39:30 +0300

    Добавлен прототип терминала

--- Assets/Scripts/Managers/CameraHovering.cs
+++ Assets/Scripts/Managers/CameraHovering.cs
@@ -5,6 +5,7 @@ public class CameraHovering : MonoBehaviour
     public Vector3 moveVector;
     public float moveMult = 1f;
     public float moveSpeed;
+    public GameObject TerminalObj;
 
     void Update()
     {
@@ -16,10 +17,10 @@ public class CameraHovering : MonoBehaviour
         if (Input.GetKey(KeyCode.D)) { if (DirectionChosen.hov_x < DirectionChosen.hov_x_cap_up) { DirectionChosen.hov_x += 1f; } }
         if (Input.GetKey(KeyCode.A)) { if (DirectionChosen.hov_x > DirectionChosen.hov_x_cap_down) { DirectionChosen.hov_x -= 1f; } }
 
-        if (Input.GetKeyUp(KeyCode.W)) {  DirectionChosen.hov_y = 0f; } 
-        if (Input.GetKeyUp(KeyCode.S)) {  DirectionChosen.hov_y = 0f; } 
-        if (Input.GetKeyUp(KeyCode.D)) {  DirectionChosen.hov_x = 0f; } 
-        if (Input.GetKeyUp(KeyCode.A)) {  DirectionChosen.hov_x = 0f; } 
+        if (Input.GetKeyUp(KeyCode.W)) { DirectionChosen.hov_y = 0f; }
+        if (Input.GetKeyUp(KeyCode.S)) { DirectionChosen.hov_y = 0f; }
+        if (Input.GetKeyUp(KeyCode.D)) { DirectionChosen.hov_x = 0f; }
+        if (Input.GetKeyUp(KeyCode.A)) { DirectionChosen.hov_x = 0f; }
 
         moveVector = new Vector3(moveSpeed * DirectionChosen.hov_x, 0, moveSpeed * DirectionChosen.hov_y);
         transform.position += moveVector;
---
+++ Assets/Scripts/Managers/MainOptionsTypingManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class MainOptionsTypingManager : MonoBehaviour
+{
+    public Text tytle;
+    public Text sysLog;
+
+    public float typingSpeed = 0.1f;
+    private string fulltytle;
+    private string fullsysLog;
+    public bool isTyping;
+    private Coroutine typingCoroutine;
+    private bool isStopped = false;
+    private int tmp;
+
+    void Start()
+    {
+        fulltytle = tytle.text;
+        fullsysLog = sysLog.text;
+        tytle.text = "";
+        sysLog.text = "";
+        isTyping = true;
+        typingCoroutine = StartCoroutine(TypeText());
+    }
+
+    void OnEnable()
+    {
+        if (tmp == 1)
+        {
+            tytle.text = "";
+            sysLog.text = "";
+            isTyping = true;
+            typingCoroutine = StartCoroutine(TypeText());
+            tmp = 0;
+        }
+    }
+
+    void OnDisable()
+    {
+        tmp = 1;
+        typingSpeed = 0.02f;
+    }
+
+    private IEnumerator TypeText()
+    {
+        for (int i = 0; i < fulltytle.Length && !isStopped; i++)
+        {
+            tytle.text += fulltytle[i];
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        for (int i = 0; i < fullsysLog.Length && !isStopped; i++)
+        {
+            sysLog.text += fullsysLog[i];
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        isTyping = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isTyping)
+            {
+                typingSpeed = 0.005f;
+            }
+        }
+    }
+}
