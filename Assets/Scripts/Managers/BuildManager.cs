adef786efb657c3e44ce2ef6e38377f252f8e73b
Alexander <realemail@trustme.net>
2025-05-18 20:50:04 +0300

    Реворк интерфейса

--- Assets/Scripts/Managers/BuildManager.cs
+++ Assets/Scripts/Managers/BuildManager.cs
@@ -26,40 +26,49 @@ public class BuildManager : MonoBehaviour
             if (obs > 0 && obs <= 50)
             {
                 obsText.color = new Color32(191, 7, 7, 255);
+                obsText.text = "Ò¸ìíèóì: ÍÈÇÊÀß";
             }
             else if (obs >= 51 && obs <= 76)
             {
                 obsText.color = new Color32(255, 196, 0, 255);
+                obsText.text = "Ò¸ìíèóì: ÑÐÅÄÍßß";
             }
             else
             {
                 obsText.color = new Color32(0, 240, 7, 255);
+                obsText.text = "Ò¸ìíèóì: ÂÛÑÎÊÀß";
             }
 
             if (ign > 0 && ign <= 50)
             {
                 ignText.color = new Color32(191, 7, 7, 255);
+                ignText.text = "Îãíåìàñëî: ÍÈÇÊÀß";
             }
             else if (ign >= 51 && ign <= 76)
             {
                 ignText.color = new Color32(255, 196, 0, 255);
+                ignText.text = "Îãíåìàñëî: ÑÐÅÄÍßß";
             }
             else
             {
                 ignText.color = new Color32(0, 240, 7, 255);
+                ignText.text = "Îãíåìàñëî: ÂÛÑÎÊÀß";
             }
 
             if (ven > 0 && ven <= 50)
             {
                 venText.color = new Color32(191, 7, 7, 255);
+                venText.text = "Òîêñèä: ÍÈÇÊÀß";
             }
             else if (ven >= 51 && ven <= 76)
             {
                 venText.color = new Color32(255, 196, 0, 255);
+                venText.text = "Òîêñèä: ÑÐÅÄÍßß";
             }
             else
             {
                 venText.color = new Color32(0, 240, 7, 255);
+                venText.text = "Òîêñèä: ÂÛÑÎÊÀß";
             }
 
             panel.gameObject.SetActive(true);
--- Assets/Scripts/Managers/WorldBorders.cs
+++ Assets/Scripts/Managers/WorldBorders.cs
@@ -5,11 +5,11 @@ public class WorldBorders : MonoBehaviour
     public static float cell_width = 1.4f;   
 
     public static float North_Border = cell_width * HexagonGeneration.layers - cell_width * 5;
-    public static float South_Border = -cell_width * HexagonGeneration.layers - cell_width * 3;
+    public static float South_Border = -cell_width * HexagonGeneration.layers - cell_width * 5;
     public static float West_Border = -cell_width * HexagonGeneration.layers;
     public static float East_Border = cell_width * HexagonGeneration.layers;
 
-    public static float Upper_Border = 24f;
+    public static float Upper_Border = 26f;
     public static float Bottom_Border = 7f;
 
     public static float CamVelDist_Mult = 1f;
