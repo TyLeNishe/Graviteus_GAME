cb78c985b21ee456a456b855db9fb4575181bcf0
Sined <denic_b03@mail.ru>
2025-05-20 00:36:59 +0300

    Сделал смену дня и ночи

---
+++ Assets/Scripts/Managers/LightingManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+    //Scene References
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private LightingPreset Preset;
+
+    //Variables
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+    [SerializeField] private float DayDuration = 96f; // Полный день за 96 секунд
+
+    private void Update()
+    {
+        if (Preset == null)
+            return;
+
+        if (Application.isPlaying)
+        {
+            // Рассчитываем приращение времени с учетом длительности дня
+            TimeOfDay += (Time.deltaTime * 24f) / DayDuration;
+            TimeOfDay %= 24; // Обеспечиваем цикл 0-24
+            UpdateLighting(TimeOfDay / 24f);
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 24f);
+        }
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+        //Set ambient and fog
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
+
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+            return;
+
+        if (RenderSettings.sun != null)
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+        else
+        {
+            Light[] lights = FindObjectsByType<Light>(FindObjectsSortMode.None);
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+}
