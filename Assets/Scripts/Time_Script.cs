ea9cbc45193506c4a9b3c050c6f6901459a39f76
Sined <denic_b03@mail.ru>
2025-05-20 01:26:32 +0300

    Сделал рандомное появление тумана

--- Assets/Scripts/Time_Script.cs
+++ Assets/Scripts/Time_Script.cs
@@ -10,10 +10,10 @@ public class Time_Script : MonoBehaviour
     public Text Hours, Minutes, Day;
 
     private float hour = 0, full_circles = 8, current_circle = 0, time_speed = 1, day_cycle = 24f, state_lenth = 48f;
-    private int clock_frame = 1, sun_state = 1; //0 - Восход, 1 - День, 2 - Закат, 3 - Ночь
+    private int clock_frame = 1, sun_state = 1; //0 - пїЅпїЅпїЅпїЅпїЅпїЅ, 1 - пїЅпїЅпїЅпїЅ, 2 - пїЅпїЅпїЅпїЅпїЅ, 3 - пїЅпїЅпїЅпїЅ
     public Image clock;
     public Light Light;
-    public static bool paused = false, weather_flag = false;
+    public static bool paused = false, weather_flag = false, fog_flag = false;
 
 
     public void X05_speed() 
@@ -50,10 +50,10 @@ public class Time_Script : MonoBehaviour
 
             if (current_circle >= full_circles) { current_circle = 0; }
 
-            if (hour >= 24f) { hour = 0.0f; weather_flag = true; } else { hour += Time.deltaTime * time_speed; day_cycle += Time.deltaTime * time_speed; }
+            if (hour >= 24f) { hour = 0.0f; weather_flag = true; fog_flag = true; } else { hour += Time.deltaTime * time_speed; day_cycle += Time.deltaTime * time_speed; }
 
             if (hour > (clock_frame + (current_circle * 16)) * (24f / 16f / full_circles) && hour <= (clock_frame + 1 + (current_circle * 16)) * (24f / 16f / full_circles)) { clock.sprite = Inactive_Sprites[clock_frame - 1]; clock_frame += 1; }
-           
+            
         }
         if (Mathf.FloorToInt(hour) < 10) { Hours.text = "0" + Mathf.FloorToInt(hour).ToString(); } else { Hours.text = Mathf.FloorToInt(hour).ToString(); }
         
